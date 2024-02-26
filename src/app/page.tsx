b76e673f38e12151a61a84dbed0882732c8d8027
Junior Martins <jospse@gmail.com>
2024-02-26 17:36:29 -0300

    added a new json file to describe the cards. Next step is to organize it into a button exibition method.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,7 @@ import Header from "@/components/header";
 import Hero from "@/components/hero";
 import Payment from "@/components/payment";
 import Safety from "@/components/safety";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
--- src/components/cardHolder/index.tsx
+++ src/components/cardHolder/index.tsx
@@ -1,41 +1,71 @@
 import Image from "next/image";
 import { Button } from "../ui/button";
 import { Separator } from "../ui/separator";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card"
 
-export default function CardHolder() {
-  return(
+interface Information {
+  id: string,
+  category: string,
+  title: string,
+  description: string,
+  image: string
+}
+
+async function getInformations(): Promise<Information[]> {
+  const result = await fetch('http://localhost:3000/informations')
+
+  return result.json()
+}
+
+export default async function CardHolder() {
+  const informations = await getInformations()
+
+  return (
     <div className="bg-white h-4/5 px-8 my-8">
-          <h1 className="text-5xl font-bold py-12 text-tertiary">
-            O Carro<span className="text-primary text-6xl">:</span>
-          </h1>
-          <ul className="flex justify-around font-bold text-primary cursor-pointer text-lg">
-            <Button variant='ghost' className="w-1/5">
-              DESIGN
-            </Button>
-            <Button variant='ghost' className="w-1/5">
-              INTERIOR
-            </Button>
-            <Button variant='ghost' className="w-1/5">
-              TECNOLOGIA
-            </Button>
-            <Button variant='ghost' className="w-1/5">
-              PERFORMANCE
-            </Button>
-            <Button variant='ghost' className="w-1/5">
-              EXTRA
-            </Button>
-          </ul>
-          <Separator className="border-4 border-tertiary mt-2" />
-          <div className="mt-8 flex flex-row">
+      <h1 className="text-5xl font-bold py-12 text-tertiary">
+        O Carro<span className="text-primary text-6xl">:</span>
+      </h1>
+      <ul className="flex justify-around font-bold text-primary cursor-pointer text-lg">
+        <Button variant='ghost' className="w-1/5">
+          DESIGN
+        </Button>
+        <Button variant='ghost' className="w-1/5">
+          INTERIOR
+        </Button>
+        <Button variant='ghost' className="w-1/5">
+          TECNOLOGIA
+        </Button>
+        <Button variant='ghost' className="w-1/5">
+          PERFORMANCE
+        </Button>
+        <Button variant='ghost' className="w-1/5">
+          EXTRA
+        </Button>
+      </ul>
+      <Separator className="border-4 border-tertiary mt-2" />
+      <div className="mt-8 flex flex-row">
+        {informations.map(information => (
+          <Card key={information.id}>
+            <div>
+              <CardHeader>
+                <CardDescription>{information.category}</CardDescription>
+                <CardTitle>{information.title}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>{information.description}</p>
+              </CardContent>
+            </div>
+          </Card>
+        ))}
 
-            <Image
-              alt="Carro: Fiat Freemont em alta velocidade"
-              width={300}
-              height={300}
-              src="/freemont.jpg"
-              className="w-1/5 mb-8"
-            />
-          </div>
-        </div>
+      </div>
+    </div>
   )
 }
